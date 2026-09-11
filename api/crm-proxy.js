9857f36046441e89d0827924b3ae6d02956199ef
pepperplastic <139880665+pepperplastic@users.noreply.github.com>
2026-09-11 14:09:32 -0400

    Add getSetting and setSetting to CRM proxy

--- api/crm-proxy.js
+++ api/crm-proxy.js
@@ -70,7 +70,7 @@ const WRITE_ACTIONS = new Set([
   'pushToLeadsOnline', 'uploadLeadsOnlinePhotos',
   'manualCustomerShipment', 'getAffiliates', 'addAffiliate', 'updateAffiliate',
   'deleteAffiliate', 'getAffiliateStats',
-  'getMarketingRoi', 'getAdSpend', 'addAdSpend', 'deleteAdSpend', 'syncMetaSpend',
+  'getMarketingRoi', 'getAdSpend', 'addAdSpend', 'deleteAdSpend', 'syncMetaSpend','getSetting','setSetting',
 ]);
 
 const UPSTREAM_TIMEOUT_MS = 55_000;
